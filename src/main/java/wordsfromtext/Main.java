01f730e02d261b2a04a80885924099e5a7148e0c
Alexey L <alexeylp12345@gmail.com>
2023-11-19 02:50:54 +0500

    resolved some problems with numbers

--- src/main/java/wordsfromtext/Main.java
+++ src/main/java/wordsfromtext/Main.java
@@ -2,9 +2,9 @@ package wordsfromtext;
 
 
 public class Main {
-    public static final String NAME_OF_TEXTFILE = "HPSorcerersStone.txt";
-    public static final int MIN_FREQUENCY = 5;
-    public static final int MAX_FREQUENCY = 1000;
+    public static final String NAME_OF_TEXTFILE = "gameOfThrones(01-05seasons).txt";
+    public static final int MIN_FREQUENCY = 3;
+    public static final int MAX_FREQUENCY = 1500;
 
     public static void main(String [] args){
         FrequencyWordsFromText.getWords(NAME_OF_TEXTFILE, MIN_FREQUENCY, MAX_FREQUENCY);
